919eaaf95c321747fafc15fdfbe98af9360b0f13
Diego Luis Mayolo <diego.mayolo@outlook.com>
2023-01-13 22:41:10 -0300

    Salvando o registro do TODO através da api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms'
+import {TodoService} from "./todo.service";
+import Todo from "./todo";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  mensagem: string = "Hello World";
+
+export class AppComponent
+{
+  form: FormGroup = new FormGroup( {
+    description : new FormControl( '' )
+  } );
+
+  constructor( private service:TodoService ){}
+
+  submit()
+  {
+    console.log( this.form.value );
+    const todo: Todo = { ...this.form.value }
+    this.service.save( todo ).subscribe( savedTodo => console.log( savedTodo ) );
+  }
 }
---
+++ src/app/todo.ts
@@ -0,0 +1,7 @@
+export default class Todo {
+  id: number;
+  description: string;
+  done: boolean;
+  createdDate: string;
+  doneDated: string;
+}
